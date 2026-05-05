26f64c75980fe26a990aa5e6a9fce6cb7fb8886c
Kudriashov Dmytro <kb252_kda@student.ztu.edu.ua>
2026-05-05 12:59:12 +0300

    Cours

---
+++ AddTovarWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+
+namespace OOPWPFProject
+{
+    /// <summary>
+    /// Логика взаимодействия для AddTovarWindow.xaml
+    /// </summary>
+    public partial class AddTovarWindow : Window
+    {
+        public AddTovarWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        private void AddMot_Click(object sender, RoutedEventArgs e)
+        {
+            int amount;
+            if (!int.TryParse(AmountTxt.Text, out amount))
+            {
+                MessageBox.Show("Некоректна кількість!");
+                return;
+            }
+            decimal price;
+            if (decimal.TryParse(PriceTovar.Text, out price)){
+                string connectionString = "Host=localhost;Port=5432;Database=Order;Username=postgres;Password=promomo999;";
+
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+
+
+
+                    string query = "INSERT INTO \"Tovar\" (\"Ім'я\", \"Категорія\", \"Ціна\",\"Кількість\") VALUES (@namep, @categ, @price,@amount)";
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@namep", nameTovar.Text);
+                    cmd.Parameters.AddWithValue("@categ", categoryProduct.Text);
+                    cmd.Parameters.AddWithValue("@price", PriceTovar.Text);
+                    cmd.Parameters.AddWithValue("@amount", amount);
+                    
+
+                    nameTovar.Clear();
+                    PriceTovar.Clear();
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Некоректна ціна!");
+            }
+
+        }
+    }
+}
+
---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OOPWPFProject
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                MessageBox.Show("Директорію створено");
+            }
+            else
+            {
+                MessageBox.Show("Директорія вже існує");
+            }
+            
+        }
+    }
+    }
